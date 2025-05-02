bcad8b0bc69ee49b5d27b22dd84b81d54373a834
Pratap Chandra kuldi <pratapkuldi489@gmail.com>
2025-05-02 12:04:06 +0530

    update approutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,8 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
-
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
-// import { appRouter } from '../src/router/appRouter';  // Updated import path
-import { appRouter } from './router/AppRouter';
+import  appRouter  from './router/AppRouter';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
